0b3bb801ba30bbe498fd2cc37736b9f2f5d33080
Stephan Kleine <stephan.kleine@gmail.com>
2015-11-06 21:44:36 +0100

    fix Homebrew URL

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ fi
 
 if [[ ! -x /usr/local/bin/brew ]]; then
     echo "Installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 if [[ ! -x /usr/local/bin/brew-cask ]]; then
